2761eacbcdf155fdb7ad8c3c9db49eed7b022b82
Eric Jensen <ejensen1@swarthmore.edu>
2020-10-29 15:00:06 -0400

    Update permissions to include new files

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -6,8 +6,8 @@
 # Scripts need to be readable and executable:
 chmod o+rx acp_plan.cgi transits.cgi get_finding_charts.pl plot_airmass.cgi print_transits.cgi 
 
-# Templates, modules, and stylesheets just need to be readable: 
-chmod o+r airmass_stylesheet.css csv_text.tmpl jquery.dataTables.css Observatories.pm page_style.css target_table.tmpl 
+# Templates, modules, javascript, and stylesheets just need to be readable: 
+chmod o+r airmass_stylesheet.css csv_text.tmpl jquery.dataTables.css Observatories.pm page_style.css target_table.tmpl aladin.html aladin-tess.js aladin_finder.html vizier_mirrors.js 
 
 # Directories need to be readable and executable, including the current dir:
 chmod o+rx finding_charts src .
