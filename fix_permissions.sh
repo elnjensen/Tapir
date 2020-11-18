0fb22d28a378c97c4935e01461358315ebfe26e2
Eric Jensen <ejensen1@swarthmore.edu>
2020-11-18 14:44:28 -0500

    Add new files to permissions script

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -4,10 +4,10 @@
 # them web-accessible after a checkout or merge:
 
 # Scripts need to be readable and executable:
-chmod o+rx acp_plan.cgi transits.cgi get_finding_charts.pl plot_airmass.cgi print_transits.cgi 
+chmod o+rx *.cgi get_finding_charts.pl
 
 # Templates, modules, javascript, and stylesheets just need to be readable: 
-chmod o+r airmass_stylesheet.css csv_text.tmpl jquery.dataTables.css Observatories.pm page_style.css target_table.tmpl aladin.html aladin-tess.js aladin_finder.html vizier_mirrors.js 
+chmod o+r airmass_stylesheet.css csv_text.tmpl jquery.dataTables.css Observatories.pm page_style.css target_table.tmpl aladin.html aladin-tess.js aladin_finder.html vizier_mirrors.js
 
 # Directories need to be readable and executable, including the current dir:
 chmod o+rx finding_charts src .
