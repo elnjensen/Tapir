43c1b99c795e29efb8feb3ce85966c20cc33855c
Eric Jensen <ejensen1@swarthmore.edu>
2021-03-22 20:36:18 -0400

    Make both owner and group execute permnissions are set

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -4,7 +4,7 @@
 # them web-accessible after a checkout or merge:
 
 # Scripts need to be readable and executable:
-chmod o+rx *.cgi get_finding_charts.pl
+chmod og+rx *.cgi get_finding_charts.pl
 
 # Templates, modules, javascript, and stylesheets just need to be readable: 
 chmod o+r airmass_stylesheet.css csv_text.tmpl jquery.dataTables.css Observatories.pm page_style.css target_table.tmpl aladin.html aladin-tess.js aladin_finder.html vizier_mirrors.js
