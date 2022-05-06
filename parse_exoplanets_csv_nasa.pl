cc8d61719305ec4d034ffae3d1713ee11abd4488
Eric Jensen <ejensen1@swarthmore.edu>
2022-05-06 17:56:17 -0400

    Start of modifying scripts for new Exoplanet Archive tables

--- parse_exoplanets_csv_nasa.pl
+++ parse_exoplanets_csv_nasa.pl
@@ -48,7 +48,7 @@
 # 2019-08-26  Change to NASA Exoplanet Archive from exoplanets.org;
 # also search through multiple ephemerides to find most accurate one. 
 # 2019-10-11: Switch to CSV for the target file
-
+# 2022-05-06: Updated for v2.0 of archive output files
 
 use Text::CSV qw( csv );
 use Getopt::Std;
@@ -61,6 +61,7 @@ use strict;
 use warnings; 
 
 use constant R_sun => 6.957E8;  # Rsun in meters per IAU
+use constant R_jup => 6.6854E7;  # Rjup in meters per IAU
 use constant AU => 149597870700;  # AU in meters per IAU
 
 # Our target file is encoded as UTF-8, so make sure everything we
@@ -69,10 +70,6 @@ binmode(STDOUT, ":utf8");
 
 # Unbuffered output, helps with tracking error messages: 
 $| = 1;
-
-# An alternate target file to pull data from in the event 
-# that data here are incomplete: 
-my $target_magnitude_file = 'exoplanet_archive_transits_full_mags.txt';
 
 # Set the -d flag ("debug") for verbose output of (some) problems.
 
@@ -115,23 +112,6 @@ my @csv_lines = @{ Text::CSV::csv(allow_whitespace => '1',
 				  headers => "auto",
 		                  )};
 
-# In addition, read in the file with the magnitudes and some other
-# params: 
-
-my @mag_lines = @{ Text::CSV::csv(in => $target_magnitude_file, 
-				   encoding => "UTF8",
-				   headers => "auto",
-		                 )}; 
-
-
-# Now @mag_lines is a list of hash references. Use it to create a lookup hash,
-# keyed by the name of the planet.  In this map function, we iterate
-# over the list, and iteratively use the name entry to create a key to
-# a hash, with the value containing the relevant line: 
-my %default_pars = map { $_->{"pl_name"} => $_ } @mag_lines; 
-
-
-
 # Make a first pass through the targets to save only those entries
 # that have sufficient information for us to calculate an ephemeris;
 # that makes the logic in the next loop cleaner. 
@@ -144,9 +124,9 @@ foreach $p (@csv_lines) {
     # Make sure it has the entries we need to calculate a transit
     # ephemeris.
     # First check for period and time of mid-transit: 
-    if (($p->{'mpl_orbper'} eq '') or ($p->{'mpl_tranmid'} eq '')) {
+    if (($p->{'pl_orbper'} eq '') or ($p->{'pl_tranmid'} eq '')) {
 	if ($DEBUG) {
-	    print STDERR "Skipping entry for planet $p->{'mpl_name'}"
+	    print STDERR "Skipping entry for planet $p->{'pl_name'}"
 		. " - period or transit mid-point missing.\n";
 	}
 	next;
@@ -195,10 +175,10 @@ while ($i < $n_good) {
 
     do {
 	push @p_list, $good_entries[$i];
-	$prev_name = $good_entries[$i]->{'mpl_name'};
+	$prev_name = $good_entries[$i]->{'pl_name'};
 	$i++; 
     } until ($i == $n_good) or ($prev_name ne
-				$good_entries[$i]->{'mpl_name'});
+				$good_entries[$i]->{'pl_name'});
 
     # Now we have the list of entries for the same system.  Find the
     # one with minimum uncertainty. 
@@ -212,20 +192,20 @@ while ($i < $n_good) {
 	my ($period_err, $midpoint_err); 
 	# If errors are missing, try to estimate from number of
 	# decimal places listed: 
-	if (($p->{'mpl_orbpererr1'} eq '') and
-	    ($p->{'mpl_orbpererr2'} eq '')) {
+	if (($p->{'pl_orbpererr1'} eq '') and
+	    ($p->{'pl_orbpererr2'} eq '')) {
 	    $period_err = ''; 
 	} else {
-	    $period_err = max($p->{'mpl_orbpererr1'},
-			      -1*$p->{'mpl_orbpererr2'}); 
+	    $period_err = max($p->{'pl_orbpererr1'},
+			      -1*$p->{'pl_orbpererr2'}); 
 	}
 
-	if (($p->{'mpl_tranmiderr1'} eq '') and
-	    ($p->{'mpl_tranmiderr2'} eq '')) {
+	if (($p->{'pl_tranmiderr1'} eq '') and
+	    ($p->{'pl_tranmiderr2'} eq '')) {
 	    $midpoint_err = '';
 	} else {
-	    $midpoint_err = max($p->{'mpl_tranmiderr1'},
-				-1*$p->{'mpl_tranmiderr2'}); 
+	    $midpoint_err = max($p->{'pl_tranmiderr1'},
+				-1*$p->{'pl_tranmiderr2'}); 
 	}
 	$p_best->{'period_err'} = $period_err;
 	$p_best->{'midpoint_err'} = $midpoint_err;
@@ -234,19 +214,19 @@ while ($i < $n_good) {
 	foreach $p (@p_list) {
 	    # Take period error as max of upper and lower entries;
 	    # Second error entry is negative, so multiply by -1.
-	    next if (($p->{'mpl_orbpererr1'} eq '') or
-		     ($p->{'mpl_orbpererr2'} eq '') or
-		     ($p->{'mpl_tranmiderr1'} eq '') or
-		     ($p->{'mpl_tranmiderr2'} eq ''));
-	    my $period_err = max($p->{'mpl_orbpererr1'},
-				 -1*$p->{'mpl_orbpererr2'}); 
-	    my $midpoint_err = max($p->{'mpl_tranmiderr1'},
-				   -1*$p->{'mpl_tranmiderr2'}); 
+	    next if (($p->{'pl_orbpererr1'} eq '') or
+		     ($p->{'pl_orbpererr2'} eq '') or
+		     ($p->{'pl_tranmiderr1'} eq '') or
+		     ($p->{'pl_tranmiderr2'} eq ''));
+	    my $period_err = max($p->{'pl_orbpererr1'},
+				 -1*$p->{'pl_orbpererr2'}); 
+	    my $midpoint_err = max($p->{'pl_tranmiderr1'},
+				   -1*$p->{'pl_tranmiderr2'}); 
 	    next if (($period_err == 0) or ($midpoint_err == 0));
 	    # Otherwise, we have the fields we need to find the
 	    # uncertainty: 
 	    my $n_periods = abs($this_jd -
-				$p->{'mpl_tranmid'})/$p->{'mpl_orbper'}; 
+				$p->{'pl_tranmid'})/$p->{'pl_orbper'}; 
 	    my $transit_unc = sqrt($midpoint_err**2 +
 				   ($n_periods*$period_err)**2); 
 	    if ($transit_unc < $min_err) {
@@ -261,7 +241,7 @@ while ($i < $n_good) {
 	}
     }
     if ($p_best eq '') {
-	print "More than one entry for $p_list[0]->{'mpl_name'}" . 
+	print "More than one entry for $p_list[0]->{'pl_name'}" . 
 	    " but none have uncertainty fields, cannot choose.\n"; 
 	exit;
     }
@@ -272,18 +252,11 @@ while ($i < $n_good) {
     # Some entries that have good periods are missing duration.  See
     # if we can fill that in. 
 
-
-    # Get the "default parameters" entry for this system: 
-    my $pars = $default_pars{$p_best->{'mpl_name'}};
-
     # Get magnitude from default params: 
-    if ($pars->{'st_optmag'} ne '') {
-	$V = $pars->{'st_optmag'};
-	if ($pars->{'st_optband'} =~ m/Kepler/) {
-	    $p_best->{'comment'} .= " Mag is Kepler band. ";
-	}
-    } elsif ($pars->{'gaia_gmag'} ne '') {
-	$V = $pars->{'gaia_gmag'};
+    if ($p_best->{'sy_vmag'} ne '') {
+	$V = $p_best->{'sy_vmag'};
+    } elsif ($p_best->{'sy_gaiamag'} ne '') {
+	$V = $p_best->{'sy_gaiamag'};
 	$p_best->{'comment'} .= " Mag is Gaia G. ";
     } else {
 	$V = -99; 
@@ -291,58 +264,52 @@ while ($i < $n_good) {
 
     
     # Also use depth from that file if we don't already have it: 
-    if ($p_best->{'mpl_trandep'} eq '') { 
-	if ($pars->{'pl_trandep'} ne '') {	
-	    $p_best->{'mpl_trandep'} = $pars->{'pl_trandep'};
-	} elsif ($pars->{'pl_ratror'} ne '') {	
+    if ($p_best->{'pl_trandep'} eq '') { 
+	if ($p_best->{'pl_ratror'} ne '') {	
 	    # Use tabulated planet-star radius ratio, convert to percent: 
-	    $p_best->{'mpl_trandep'} = 100 * ($pars->{'pl_ratror'})**2;
-	} elsif (($pars->{'pl_rads'} ne '') and ($pars->{'st_rad'} ne '')) {	
+	    $p_best->{'pl_trandep'} = 100 * ($p_best->{'pl_ratror'})**2;
+	} elsif (($p_best->{'pl_radj'} ne '') and ($p_best->{'st_rad'} ne '')) {	
 	    # Calculate and use planet-star radius ratio, convert to percent: 
-	    $p_best->{'mpl_trandep'} = 100 * ($pars->{'pl_rads'}/$pars->{'st_rad'})**2;
+	    $p_best->{'pl_trandep'} = 100 * ($p_best->{'pl_radj'}*R_jup/($p_best->{'st_rad'}*R_sun))**2;
 	}      
     }
 
-    if ($p_best->{'mpl_trandur'} eq '') {
-	# First try the 'default params' file: 
-	if  ($pars->{'pl_trandur'} ne '') {	
-	    $p_best->{'mpl_trandur'} = $pars->{'pl_trandur'};
-	} else {
-	    # Didn't find it there, loop over other entries to see if
-	    # we can find one with a duration. 
-	    my $dur_found = 0;
-	    my @planet_list = @p_list;
+    if ($p_best->{'pl_trandur'} eq '') {
+	# Didn't find it there, loop over other entries to see if
+	# we can find one with a duration. 
+	my $dur_found = 0;
+	my @planet_list = @p_list;
+	while ((not $dur_found) and (scalar(@planet_list) > 0)) {
+	    $p = pop @planet_list;
+	    if ($p->{'pl_trandur'} ne '') {
+		$p_best->{'pl_trandur'} = $p->{'pl_trandur'};
+		$dur_found = 1;
+	    }
+	}
+	if (not $dur_found) {
+	    # Try again, now with estimating duration from other
+	    # parameters. 
+	    @planet_list = @p_list;
 	    while ((not $dur_found) and (scalar(@planet_list) > 0)) {
 		$p = pop @planet_list;
-		if ($p->{'mpl_trandur'} ne '') {
-		    $p_best->{'mpl_trandur'} = $p->{'mpl_trandur'};
+		my ($duration, $status, $comment) = estimate_duration($p);
+		if ($status) {
+		    $p_best->{'pl_trandur'} = $duration;
+		    $p_best->{'comment'} .= $comment;
 		    $dur_found = 1;
 		}
 	    }
 	    if (not $dur_found) {
-		# Try again, now with estimating duration from other
-		# parameters. 
-		@planet_list = @p_list;
-		while ((not $dur_found) and (scalar(@planet_list) > 0)) {
-		    $p = pop @planet_list;
-		    my ($duration, $status, $comment) = estimate_duration($p);
-		    if ($status) {
-			$p_best->{'mpl_trandur'} = $duration;
-			$p_best->{'comment'} .= $comment;
-			$dur_found = 1;
-		    }
+		# Could not get duration at all! 
+		if ($DEBUG) {
+		    print STDERR "### Could not get duration for $p_best->{'pl_name'}!\n";
 		}
-		if (not $dur_found) {
-		    # Could not get duration at all! 
-		    if ($DEBUG) {
-			print STDERR "### Could not get duration for $p_best->{'mpl_name'}!\n";
-		    }
-		    next PLANETS;
-		}
-
+		next PLANETS;
 	    }
+
 	}
     }
+
 
     # Just to make what's below less verbose: 
     $p = $p_best;
@@ -350,27 +317,27 @@ while ($i < $n_good) {
 
     # Sometimes no depth is given - try to estimate depth from planet
     # and stellar radii:
-    if ( $p->{'mpl_trandep'} eq '' ) {
+    if ( $p->{'pl_trandep'} eq '' ) {
 	# 
 	if ($DEBUG) {
-	    print STDERR "No depth given for $p->{'mpl_name'}.\n";
-	    print STDERR "Estimating depth for $p->{'mpl_name'}\n$p\n";
+	    print STDERR "No depth given for $p->{'pl_name'}.\n";
+	    print STDERR "Estimating depth for $p->{'pl_name'}\n$p\n";
 	}
 
 	# Give depth in ppt:
-	if ($p->{'mpl_ratror'} ne '') {
-	    $depth_ppt = 1000 * $p->{'mpl_ratror'}**2;
-	} elsif (($p->{'mpl_rads'} ne '') and ($p->{'mst_rad'} ne '')) {
-	    $depth_ppt = 1000 * ($p->{'mpl_rads'}/$p->{'mst_rad'})**2;
+	if ($p->{'pl_ratror'} ne '') {
+	    $depth_ppt = 1000 * $p->{'pl_ratror'}**2;
+	} elsif (($p->{'pl_radj'} ne '') and ($p->{'st_rad'} ne '')) {
+	    $depth_ppt = 1000 * (R_jup * $p->{'pl_radj'}/(R_sun * $p->{'st_rad'}))**2;
 	} else {
-	    print STDERR "Could not estimate depth for $p->{'mpl_name'}\n";
+	    print STDERR "Could not estimate depth for $p->{'pl_name'}\n";
 	    $depth_ppt = -99;
 	}
 
     } else {
 	# Convert depth to ppt.  Depth is given as percentage (parts
 	# per hundred) in catalog: 
-	$depth_ppt = 10 * $p->{'mpl_trandep'};
+	$depth_ppt = 10 * $p->{'pl_trandep'};
     }
 
     # Only write out the +/- separator in cases where the
@@ -394,32 +361,32 @@ while ($i < $n_good) {
 	$depth_ppt_string = sprintf('%0.1f', $depth_ppt);
     }
 
-    $duration_hours = sprintf('%0.2f', 24. * $p->{'mpl_trandur'});
+    $duration_hours = sprintf('%0.2f', 24. * $p->{'pl_trandur'});
     
     # RA and Dec strings use hms and dms, change to colons for those
     # in between, strip trailing 's': 
-    $p->{'ra_str'} =~ s/[hmd]/:/g;
-    $p->{'ra_str'} =~ s/s//;
-    $p->{'dec_str'} =~ s/[hmd]/:/g;
-    $p->{'dec_str'} =~ s/s//;
+    $p->{'rastr'} =~ s/[hmd]/:/g;
+    $p->{'rastr'} =~ s/s//;
+    $p->{'decstr'} =~ s/[hmd]/:/g;
+    $p->{'decstr'} =~ s/s//;
     
     # # Print the final output line:
-    # print $p->{'mpl_name'} . $sep . $p->{'ra_str'} .
+    # print $p->{'pl_name'} . $sep . $p->{'ra_str'} .
     # 	$sep . $p->{'dec_str'} . $sep . $V .
-    # 	$sep . $p->{'mpl_tranmid'} . $transit_error_sep .
-    # 	$p->{'midpoint_err'} . $sep . $p->{'mpl_orbper'} . $period_error_sep .
+    # 	$sep . $p->{'pl_tranmid'} . $transit_error_sep .
+    # 	$p->{'midpoint_err'} . $sep . $p->{'pl_orbper'} . $period_error_sep .
     # 	$p->{'period_err'} . $sep . $duration_hours . $sep . 
     # 	$p->{'comment'} . $sep . $priority . $sep . 
     # 	$depth_ppt_string . "\n";
 
     my @line = (
-	$p->{'mpl_name'},
-	$p->{'ra_str'},
-	$p->{'dec_str'}, 
+	$p->{'pl_name'},
+	$p->{'rastr'},
+	$p->{'decstr'}, 
 	$V,
-	$p->{'mpl_tranmid'}, 
+	$p->{'pl_tranmid'}, 
 	$p->{'midpoint_err'}, 
-	$p->{'mpl_orbper'}, 
+	$p->{'pl_orbper'}, 
 	$p->{'period_err'}, 
 	$duration_hours,
 	$p->{'comment'}, 
@@ -475,43 +442,43 @@ sub estimate_duration {
     # Mallen-Ornelas 2003
 
     my $a_over_r = '';
-    if (($p->{'mpl_orbsmax'} ne '') and 
-	($p->{'mst_rad'} ne '')) {
-	$a_over_r = ($p->{'mpl_orbsmax'} * AU) / ($p->{'mst_rad'}
+    if (($p->{'pl_orbsmax'} ne '') and 
+	($p->{'st_rad'} ne '')) {
+	$a_over_r = ($p->{'pl_orbsmax'} * AU) / ($p->{'st_rad'}
 						  * R_sun); 
     } else {
 	# Not quite the same for non-zero e, since this is defined
 	# as "The distance between the planet and the star at
 	# mid-transit divided by the stellar radius."  Should be
 	# close in most cases. 
-	$a_over_r = $p->{'mpl_ratdor'};
+	$a_over_r = $p->{'pl_ratdor'};
     }
 
-    my $rplanet_over_rstar = $p->{'mpl_ratror'};
+    my $rplanet_over_rstar = $p->{'pl_ratror'};
     # Even though the above field is defined, sometimes it's not
     # filled in even though r_planet and r_star are both given: 
     if ($rplanet_over_rstar eq '') {
-	if (($p->{'mpl_rads'} ne '') and 
-	    ($p->{'mst_rad'} ne '')) {
-	    $rplanet_over_rstar = $p->{'mpl_rads'} / $p->{'mst_rad'};
+	if (($p->{'pl_radj'} ne '') and 
+	    ($p->{'st_rad'} ne '')) {
+	    $rplanet_over_rstar = R_jup * $p->{'pl_radj'} / (R_sun * $p->{'st_rad'});
 	}
     }
 
     # Get impact parameter if not already given: 
-    if ($p->{'mpl_imppar'} eq '') {
-	if (($p->{'mpl_orbincl'} ne '') and 
+    if ($p->{'pl_imppar'} eq '') {
+	if (($p->{'pl_orbincl'} ne '') and 
 	    ($a_over_r ne '')) {
-	    $p->{'mpl_imppar'} = $a_over_r *
-		abs(cos(deg2rad($p->{'mpl_orbincl'})));
+	    $p->{'pl_imppar'} = $a_over_r *
+		abs(cos(deg2rad($p->{'pl_orbincl'})));
 	}  else {
 	    # No inclination, so punt and assume transit across
 	    # the middle of the star so we can at least get
 	    # ballpark duration:
-	    $p->{'mpl_imppar'} = 0;
+	    $p->{'pl_imppar'} = 0;
 	}
     }
 
-    if (($p->{'mpl_imppar'} eq '') 
+    if (($p->{'pl_imppar'} eq '') 
 	or ($a_over_r eq '') or ($a_over_r < 1)
 	or ($rplanet_over_rstar eq '') 
 	or ($rplanet_over_rstar > 1)) {
@@ -524,14 +491,14 @@ sub estimate_duration {
 
     
     my $sqrt_term = (1 + $rplanet_over_rstar)**2
-	- $p->{'mpl_imppar'}**2;
+	- $p->{'pl_imppar'}**2;
     if ($sqrt_term < 0) {
 	if ($DEBUG) {
-	    print STDERR "Skipping entry for planet $p->{'mpl_name'}"
+	    print STDERR "Skipping entry for planet $p->{'pl_name'}"
 		. " - cannot calculate duration, sqrt term is negative.\n";
 	    print STDERR "R planet over Rstar: $rplanet_over_rstar\n";
 	    print STDERR "a over R: $a_over_r\n";
-	    print STDERR "Impact parameter: $p->{'mpl_imppar'}\n"; 
+	    print STDERR "Impact parameter: $p->{'pl_imppar'}\n"; 
 	    print STDERR $p, "\n\n";
 	}
 	# can't do the calculation; return
@@ -540,7 +507,7 @@ sub estimate_duration {
 	undef $duration; 
 	return ($duration, $status, $comment); 
     }
-    $duration = ($p->{'mpl_orbper'} / ($a_over_r * pi))
+    $duration = ($p->{'pl_orbper'} / ($a_over_r * pi))
 	* sqrt($sqrt_term); 
     $comment = "Duration estimated. "; 
     $status = 1; 
