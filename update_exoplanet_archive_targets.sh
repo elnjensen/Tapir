a82590ca488dc59e8a2841038e2c1870e47b4430
Eric Jensen <ejensen1@swarthmore.edu>
2022-05-12 15:29:54 -0400

    Update script for new Exoplanet Archive tables

--- update_exoplanet_archive_targets.sh
+++ update_exoplanet_archive_targets.sh
@@ -3,14 +3,15 @@
 # Script to download the NASA Exoplanet Archive database for processing to 
 # make a target list for the public transit finder. 
 # Eric Jensen, 2019-08-26
+# Revised for new Exoplanet Archive tables, 05-06-2022
 
 # Filename base for target file: 
 TARG_BASE=exoplanet_archive_transits_full
 
 NEW_FILE=${TARG_BASE}_new.txt
 TARG_FILE=${TARG_BASE}.txt
-MAG_FILE=${TARG_BASE}_mags.txt
-MAG_FILE_TEMP=${TARG_BASE}_mags_temp.txt
+COMPOSITE_FILE=${TARG_BASE}_composite.txt
+COMPOSITE_FILE_TEMP=${TARG_BASE}_composite_temp.txt
 
 OUTPUT_FILE=transit_targets.csv
 
@@ -20,34 +21,35 @@ DIR=/home/httpd/html/transits/
 cd $DIR
 
 # Get file, and sort it by all except the first line.  Save as a temporary file: 
-curl --silent --fail 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exomultpars&select=mpl_name,mpl_orbper,mpl_orbpererr1,mpl_orbpererr2,mpl_orbsmax,mpl_orbincl,ra_str,dec_str,mst_rad,mpl_rads,mpl_trandep,mpl_trandur,mpl_trandurerr1,mpl_trandurerr2,mpl_tranmid,mpl_tranmiderr1,mpl_tranmiderr2,mpl_imppar,mpl_ratdor,mpl_ratror,mpl_tsystemref&where=(mpl_tranflag=1)' | awk 'NR<2{ print; next }{ print | "sort" }' > $NEW_FILE
+curl --silent --fail 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_orbper,pl_orbpererr1,pl_orbpererr2,pl_orbsmax,pl_orbincl,rastr,decstr,ra,dec,st_rad,pl_radj,pl_trandep,pl_trandur,pl_trandurerr1,pl_trandurerr2,pl_tranmid,pl_tranmiderr1,pl_tranmiderr2,pl_imppar,pl_ratdor,pl_ratror,sy_vmag,sy_gaiamag+from+ps+where+tran_flag=1&format=csv' | awk 'NR<2{ print; next }{ print | "sort" }' > $NEW_FILE
 
 
 # If non-zero size and different from the old file, replace the old file: 
-if [ -s $NEW_FILE ] && ! cmp --silent $TARG_FILE $NEW_FILE
+if [ -s $NEW_FILE ] && (! [ -s $OUTPUT_FILE ] || ! cmp --silent $TARG_FILE $NEW_FILE)
 then
-    # Files are different:
+    # Files are different, or we don't have a target file at all. 
     # Echo differences; comment out next line if you want silent output:
     diff $TARG_FILE $NEW_FILE
     # Replace the old file: 
     mv $NEW_FILE $TARG_FILE
     chmod o+r $TARG_FILE 
-    # Also download the supplemental information from the other table: 
-    curl --silent --fail 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,st_optmag,st_optband,gaia_gmag,pl_trandur,pl_trandep,pl_rads,st_rad,pl_ratror&where=(pl_tranflag=1)' | awk 'NR<2{ print; next }{ print | "sort" }' > $MAG_FILE_TEMP
-    if [ -s $MAG_FILE_TEMP ]
+
+    curl --silent --fail 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_orbper,pl_orbpererr1,pl_orbpererr2,pl_orbsmax,pl_orbincl,rastr,decstr,ra,dec,st_rad,pl_radj,pl_trandep,pl_trandur,pl_trandurerr1,pl_trandurerr2,pl_tranmid,pl_tranmiderr1,pl_tranmiderr2,pl_imppar,pl_ratdor,pl_ratror,sy_vmag,sy_gaiamag+from+pscomppars+where+tran_flag=1&format=csv' | awk 'NR<2{ print; next }{ print | "sort" }' > $COMPOSITE_FILE_TEMP
+    if [ -s $COMPOSITE_FILE_TEMP ]
     then
-	mv $MAG_FILE_TEMP $MAG_FILE
-	chmod o+r $MAG_FILE
+	mv $COMPOSITE_FILE_TEMP $COMPOSITE_FILE
+	chmod o+r $COMPOSITE_FILE
 
 	# Make sure both contain the same targets: 
-	diff <(perl -na -F"," -e 'print "$F[0]\n" if ($. > 1)' $TARG_FILE |uniq) <(perl -na -F"," -e 'print "$F[0]\n" if ($. > 1)' $MAG_FILE |uniq)
+	diff <(perl -na -F"," -e 'print "$F[0]\n" if ($. > 1)' $TARG_FILE |uniq) <(perl -na -F"," -e 'print "$F[0]\n" if ($. > 1)' $COMPOSITE_FILE |uniq)
+
 
 	# Have both files, do the processing: 
 	./parse_exoplanets_csv_nasa.pl $TARG_FILE > $OUTPUT_FILE
 	chmod o+r  $OUTPUT_FILE
     else
-	/bin/rm -f $MAG_FILE_TEMP
-	echo "Successfully downloaded target file, but failed to get magnitudes file."
+	/bin/rm -f $COMPOSITE_FILE_TEMP
+	echo "Successfully downloaded target file, but failed to get composite parameters file."
 	exit
     fi 
 else
